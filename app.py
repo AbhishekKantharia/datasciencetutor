91fbf41cd5d370ebd2633e8c265ab30799bef8ad
AbhishekKantharia <61312706+AbhishekKantharia@users.noreply.github.com>
2025-02-13 20:36:26 +0530

    Update app.py
    Updated the code to work on Local and Production Respectively.

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-import os
 import streamlit as st
 import google.generativeai as ai
 from dotenv import load_dotenv
+import os
 
-api_key = os.getenv("GOOGLE_API_KEY")  # Retrieve the API key from environment variables
+load_dotenv()  # Load environment variables from .env file
+
+api_key = os.getenv("GOOGLE_API_KEY")  
 
 if not api_key:
-    st.error("API Key is missing! Set the GOOGLE_API_KEY environment variable.")
+    st.error("API Key is missing! Please set it in the .env file.")
     st.stop()
 
 ai.configure(api_key=api_key)
