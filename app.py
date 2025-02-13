d82f5c20c8794db7253f75e1e4e0a8474a53dff2
AbhishekKantharia <61312706+AbhishekKantharia@users.noreply.github.com>
2025-02-13 20:49:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,7 @@ import streamlit as st
 import google.generativeai as ai
 import os
 
-api_key = os.getenv("GOOGLE_API_KEY")  
-
-if not api_key:
-    st.error("API Key is missing! Please set it in the .env file.")
-    st.stop()
-
-ai.configure(api_key=api_key)
+ai.configure(api_key="AIzaSyAtlJDZ6nAr13lUfeUNx4R4v7hoZkoYY-Q")
 
 # System prompt for the AI model
 sys_prompt = """You are a helpful AI Tutor for Data Science. 
